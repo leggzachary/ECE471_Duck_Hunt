7b49e57b8b5028cef3d142bc177b99184a7e0771
Zachary <leggzachary@gmail.com>
2022-04-05 12:31:02 -0700

    Update coordinate handling. Deleted duckhunt main without mp.

--- duck-hunt/solution.py
+++ duck-hunt/solution.py
@@ -4,16 +4,19 @@ import numpy as np
 """
 ECE 471 Duck Hunt Project Solution
 Zachary Legg - V00156093
+
+April 5, 2022
 """
 
 # Image size should be (1024, 768, 3), but for some reason it comes transposed 
 # so we transpose it to resize to (768, 1024, 3)
 
 last_img = np.zeros((768, 1024))
-
+toggle = 0
 
 def GetLocation(move_type, env, current_frame):
     global last_img
+    global toggle
     move_type = "absolute"
     
     #print(current_frame.shape)
@@ -34,14 +37,15 @@ def GetLocation(move_type, env, current_frame):
     if np.sum(last_img) != 0:
         c = 20
         if len(h[0]) > 0 and len(h[1]) > 0:
-            print(np.amax(diff)) 
             coordinates.append({'coordinate' : (h[1][0], h[0][0]), 'move_type' : move_type})
-            
-            coordinates.append({'coordinate' : (h[1][0] + c, h[0][0] + c), 'move_type' : move_type})
-            coordinates.append({'coordinate' : (h[1][0] + c, h[0][0] - c), 'move_type' : move_type})
-            coordinates.append({'coordinate' : (h[1][0] - c, h[0][0] + c), 'move_type' : move_type})
-            coordinates.append({'coordinate' : (h[1][0] - c, h[0][0] - c), 'move_type' : move_type})
-            
+            if toggle == 5:
+                coordinates.append({'coordinate' : (h[1][0] + c, h[0][0] + c), 'move_type' : move_type})
+                coordinates.append({'coordinate' : (h[1][0] + c, h[0][0] - c), 'move_type' : move_type})
+                coordinates.append({'coordinate' : (h[1][0] - c, h[0][0] + c), 'move_type' : move_type})
+                coordinates.append({'coordinate' : (h[1][0] - c, h[0][0] - c), 'move_type' : move_type})
+                toggle = 0 
+            else:
+                toggle = toggle + 1
             #coordinates.append({'coordinate' : (width, height), 'move_type' : "absolute"})
            
     last_img = current_img
